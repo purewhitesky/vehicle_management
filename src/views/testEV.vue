794342ee9183fee7ccdd18cce26e03613c187374
purewhite <cjifug@gmail.com>
2023-07-03 09:11:14 +0800

    updata 充電站資料顯示

--- src/views/testEV.vue
+++ src/views/testEV.vue
@@ -6,13 +6,13 @@ import "@tomtom-international/web-sdk-maps/dist/maps.css";
 import * as turf from "@turf/turf";
 import BaseButton from "@/components/BaseButton.vue";
 import { ref, reactive, onMounted } from "vue";
+import { mdiEvStation } from "@mdi/js";
 const TOMTOMKEY = "DGEne3GZIqPKvLGIxmB8xszfh0BU8NEx";
 const mapRef = ref(null);
 const center = { lat: 25.034228, lng: 121.563995 };
-const isClick = ref(false);
-const isOpen = ref(false);
+const isEVOpen = ref(false);
 onMounted(() => {
-  window.setInterval(() => {
+  /*window.setInterval(() => {
     //中心點更新
     console.log(isOpen.value);
     navigator.geolocation.getCurrentPosition((position) => {
@@ -27,16 +27,24 @@ onMounted(() => {
         lng: locationGPS.value.lng,
       });
     }
-    console.log(locationGPS.value);*/
-  }, 10000);
+    console.log(locationGPS.value);
+  }, 10000);*/
   let map = tt.map({
     key: TOMTOMKEY,
     container: mapRef.value,
-    zoom: 10,
+    zoom: 13,
     center: center,
   });
   map.addControl(new tt.FullscreenControl());
   map.addControl(new tt.NavigationControl());
+  map.addControl(
+    new tt.GeolocateControl({
+      positionOptions: {
+        enableHighAccuracy: true,
+      },
+      trackUserLocation: true,
+    })
+  );
   map.on("click", (event) => {
     const position = event.lngLat;
     console.log(position);
@@ -47,20 +55,30 @@ onMounted(() => {
   window.map = map;
 });
 
-const positionEV = ref();
+const EVNumber = ref([]);
 const openEVstation = () => EVstation(map.getBounds()._sw, map.getBounds()._ne);
-
 const EVstation = (sw, ne) =>
   tts.services
     .fuzzySearch({
       key: TOMTOMKEY,
-      query: "station",
-      connectorSet: "Tesla,Chademo,IEC62196Type2Outlet",
+      query: "",
+      categorySet: "7309",
+      connectorSet:
+        "Tesla,Chademo,IEC62196Type2Outlet,IEC62196Type1,IEC62196Type1CCS",
       boundingBox: [sw, ne],
     })
+    /*
+      IEC62196Type2Outlet：这是一种符合台湾标准的类型2电动汽车充电桩插座。
+      Chademo：这是一种快速充电标准，主要由日本汽车制造商使用。
+      Tesla：特斯拉（Tesla）是一家电动汽车制造商，他们的充电桩使用特斯拉自己的充电标准。
+      IEC62196Type1：IEC62196Type1是一种标准的交流充电插头，也被称为"J1772"，在日本用于一些电动车型的充电需求。
+      IEC62196Type1CCS：这是IEC62196Type1插头与CCS（Combined Charging System）标准的结合，支持直流快速充电和交流充电。在日本，一些新款电动车型也采用了这种充电插头类型。
+    */
     .then(function (response) {
-      console.log(response.results);
-      positionEV.value = response;
+      if (isEVOpen.value) {
+        console.log("Open");
+      }
+      //console.log(response.results);
       /*var chargingStationID = response.results[0].entryPoints.info;
       tts.services
         .evChargingStationsAvailability({
@@ -68,23 +86,101 @@ const EVstation = (sw, ne) =>
           chargingAvailability: "124599002464658",
         })
         .then(callbackFn);*/
-
       response.results.forEach((marker, index) => {
-        new tt.Marker().setLngLat(marker.position).addTo(window.map);
-        console.log(marker.position);
+        iconElement(marker, index, 0, isEVOpen.value);
       });
+      //console.log(EVNumber.value);
+      isEVOpen.value = true;
     });
+
+let markerHeight = 30,
+  markerRadius = 10,
+  linearOffset = 25;
+let popupOffsets = {
+  top: [0, 0],
+  "top-left": [0, 0],
+  "top-right": [0, 0],
+  bottom: [0, -markerHeight],
+  "bottom-left": [
+    linearOffset,
+    (markerHeight - markerRadius + linearOffset) * -1,
+  ],
+  "bottom-right": [
+    -linearOffset,
+    (markerHeight - markerRadius + linearOffset) * -1,
+  ],
+  left: [markerRadius, (markerHeight - markerRadius) * -1],
+  right: [-markerRadius, (markerHeight - markerRadius) * -1],
+};
+
+function iconElement(marker, index, iconlistNum, isOpen) {
+  if (isOpen) {
+    removeMarker(EVNumber.value);
+  }
+  //console.log(marker);
+  const svgElement = document.createElementNS(
+    "http://www.w3.org/2000/svg",
+    "svg"
+  );
+  svgElement.setAttribute("viewBox", "0 0 24 24");
+  svgElement.setAttribute("width", "30"); // 設定寬度為 200 像素
+  svgElement.setAttribute("height", "30"); // 設定高度為 150 像素
+
+  // 創建 SVG 元素的內容 - path 元素
+  const pathElement = document.createElementNS(
+    "http://www.w3.org/2000/svg",
+    "path"
+  );
+  pathElement.setAttribute("d", mdiEvStation);
+  //pathElement.setAttribute("fill", "bg-sky-400");
+
+  // 將 path 元素添加到 SVG 元素中
+  svgElement.appendChild(pathElement);
+
+  // 將 SVG 元素添加到文檔中的 body 元素中
+  document.body.appendChild(svgElement);
+  //console.log(svgElement);
+
+  /*EVNumber.value[index] = new tt.Marker({ element: svgElement })
+    .setLngLat(marker.position)
+    .addTo(window.map);*/
+
+  EVNumber.value[index] = new tt.Marker()
+    .setLngLat(marker.position)
+    .addTo(window.map);
+
+  const popup = new tt.Popup({ offset: popupOffsets }).setHTML(
+    `${marker.poi.name}
+    <br>RatedPowerKW：${marker.chargingPark.connectors[0].ratedPowerKW}
+    <br>CurrentType：${marker.chargingPark.connectors[0].currentType}
+    `
+  );
+  EVNumber.value[index].setPopup(popup);
+}
+
+const removeMarker = () => {
+  EVNumber.value.forEach((marker, index) => {
+    EVNumber.value[index].remove();
+  });
+  EVNumber.value = [];
+  console.log("remove");
+  console.log(EVNumber.value);
+
+  isEVOpen.value = false;
+};
 </script>
 <template>
   <LayoutAuthenticated>
+    <BaseButton
+      class="w-[100%]"
+      label="查詢此區域的充電樁"
+      @click="openEVstation()"
+    ></BaseButton>
+
     <div
-      class="h-[100vh] w-auto lg:h-[100vh] lg:w-[100%]"
+      class="h-[80vh] w-auto lg:h-[80vh] lg:w-[100%]"
       id="map"
       ref="mapRef"
     ></div>
-    <!--
-    <BaseButton label="1" @click="changeLocation(0)"></BaseButton>
--->
-    <BaseButton label="2" @click="openEVstation()"></BaseButton>
   </LayoutAuthenticated>
 </template>
